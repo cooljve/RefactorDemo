5d93d93d5f6427680e686d41ab577fa386f37523
Joi Liu <547169735@qq.com>
2019-08-19 21:11:23 +0800

    [JOI]:refactor:modify updateCase1,extract methods updateBackstageQuality and addOneQuality

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -25,48 +25,49 @@ public class GildedRose {
                         }
                     }
                 } else {
-                    item.quality = item.quality - item.quality;
+                    item.quality = 0;
                 }
             } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
+                addOneQuality(item);
             }
         }
     }
 
     private void updateCase2(Item item) {
         if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.sellIn = item.sellIn - 1;
+            item.sellIn--;
         }
     }
 
     private void updateCase1(Item item) {
-        if (!item.name.equals("Aged Brie")
-            && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        if ((item.name.equals("Aged Brie")
+            || item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
+            && item.quality < 50) {
+            item.quality++;
+            updateBackstageQuality(item);
+        } else {
             if (item.quality > 0) {
                 if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.quality = item.quality - 1;
+                    item.quality--;
                 }
             }
-        } else {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
+        }
+    }
 
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-                }
+    private void updateBackstageQuality(Item item) {
+        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.sellIn < 11) {
+                addOneQuality(item);
             }
+            if (item.sellIn < 6) {
+                addOneQuality(item);
+            }
+        }
+    }
+
+    private void addOneQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
         }
     }
 }
